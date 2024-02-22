ae756808ae395e9dc81555655fd7c4fff4fb7db9
Bharat8989 <kadamb208@gmail.com>
2024-02-22 20:15:14 +0530

    opeators and conditional statements

---
+++ lect2/first.js
@@ -0,0 +1,57 @@
+/* athimatic opeators*/
+// let a=13;
+// let b=3;
+// let c=a+b;
+// console.log("addtions of two number:" ,c );
+// console.log("a+b:",a+b);
+// console.log("a-b:",a-b);
+// console.log("a*b:",a*b);
+// console.log("a/b:",a/b);
+
+// // used of arithmetic opeatrors
+// console.log("a%b:",a%b);//modulus
+// console.log("a ** b:",a**b);//exponentiations
+// //unary opeator
+// console.log("a=",a ,"& b=",b);
+// a=a+1;//14
+// //a++; post increment
+// b--;//2
+// //b=b-1;
+// //++a; pre increment
+// console.log("a=a+1;",a=a+1);
+// console.log("b--:",b--);
+
+
+/*------------------------> */
+//assignment operator
+let a=9;
+let b=3;
+a +=3;
+console.log("a=",a);
+
+//comparison operators
+let c=45;
+let d=3;
+console.log("45==3:" ,c!=d );
+let k="45";
+console.log("c==k",c===k);//true
+console.log("c==k",c==k);//false
+console.log("c<=k",c<=k);
+
+//logical operators
+let o=4;
+let p=3;
+let cond1=a >b;
+let cond2=o===5;
+console.log("cond1 && cond2: ",cond1 && cond2);
+
+//conditional statements
+let age =3;
+if(age>=18){
+    console.log("you can vote");
+
+}
+if(age<18){
+    console.log("you cannot vote");
+}
+
