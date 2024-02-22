3ca7ecd754444dc58ba2a995ff1cae56558f3e31
Bharat8989 <kadamb208@gmail.com>
2024-02-22 10:24:01 +0530

    javascript variables and data types

---
+++ lect1/first.js
@@ -0,0 +1,54 @@
+console.log("hello Bhart");
+var a=30;
+console.log(a);
+fullName="tony stark";
+console.log(fullName);//tony stark
+console.log("full name");//full name
+let b=20;
+let c="bharat kadam";
+let d=null;
+
+let e=0.32;
+console.log(typeof a,typeof b,typeof c,typeof d,typeof e);
+let name="bhara kadam"
+let age =34;
+let total =344;
+console.log(name);
+console.log(age);
+var y=33;
+var x=34;
+console.log(a+b);
+var x=34;
+const t=43;
+var u=t+1;
+console.log(t);
+console.log(u);
+{
+    //block
+    let a=34;
+    console.log(a);
+}
+{
+    let a=90;
+    console.log(a);
+}
+console.log("data types in javascript")
+let q=20;
+console.log(typeof a);
+let w="bharart";
+let isfollow=true;
+let r=undefined;
+let o=null;
+let s=BigInt("123");
+let v=Symbol("bharat")
+console.log(typeof q,typeof w,typeof isfollow,typeof r,typeof o,typeof s,typeof v)
+
+const student ={ //object sections 
+       fullName :"Bharat kadam",
+       age :34,
+       cgpa:8.4,
+       ispass:true,
+
+};
+console.log(student);
+console.log(student["age"]);
