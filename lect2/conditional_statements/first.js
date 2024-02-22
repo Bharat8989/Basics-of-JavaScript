468af3eb6b71c6ef33f37173be56c1f30e81e987
Bharat8989 <kadamb208@gmail.com>
2024-02-22 22:22:40 +0530

    adding fils of prompt and alert

--- lect2/conditional_statements/first.js
+++ lect2/conditional_statements/first.js
@@ -1,1 +1,29 @@
+//conditional statements
+//if else conditional
+// let age=34;
+// if(age >= 18){
+//     console.log("vote");
 
+// }
+// else{
+//     console.log("not vote");
+// }
+//else if statements
+if(age<18)
+{
+    console.log("junior");
+
+}
+else if(age>60)
+{
+    console.log("senior");
+
+}
+else{
+   console.log("midddle");
+
+}
+//ternary opeator
+let age_=23;
+let result= age>19 ? "adult" : "not adult";
+console.log("result:",result);
---
+++ lect2/conditional_statements/practice/first.js
@@ -0,0 +1,13 @@
+// alert("hello!");//message for opne the site one time popof
+// let name=prompt("hello!");
+// console.log(name);
+/*
+   get user to input a number using promt("enter a number:").Check if the number is a multiple of 5 or not.
+*/
+let num = prompt("enter a number:");
+if(num % 5===0){
+    console.log(num, "is multiple of 5");
+}
+else{
+    console.log(num, "is not a multiple of 5");
+}
