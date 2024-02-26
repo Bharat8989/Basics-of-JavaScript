37388b48d63c2d84379e551b6595ca0f66f487d8
Bharat8989 <kadamb208@gmail.com>
2024-02-26 11:37:27 +0530

    adding practice questions sol

--- lect3/practice2/first.js
+++ lect3/practice2/first.js
@@ -1,1 +1,5 @@
 /* prompt the user to enter their full name. generate  a username for them based on the input. start username with @, followe by theri fulll name eith the fullanee lenght */
+let fullName = prompt("Enter your fullName without spaces:");
+let username = "@" + fullName + fullName.length;
+// console.log(fullName);
+console.log(username);
