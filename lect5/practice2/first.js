cba1a7eb2531b2bca24d2c6fcd05e7ff5a6fe009
Bharat8989 <kadamb208@gmail.com>
2024-03-23 23:43:09 +0530

    adding files for lect5

--- lect5/practice2/first.js
+++ lect5/practice2/first.js
@@ -1,5 +1,5 @@
 /* take a number n as input from user create an array of numbers from 1 to n . user the reduce method to calculate sum of all numbers in the array. */
-let n = prompt("Enter a number:");
+let n = prompt("enter the number:");
 let arr = [];
 
 for (let i = 1; i <= n; i++) {
